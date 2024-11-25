39da73656acb861a3f43dab88581d66465adf3ac
Melissa Pignalosa <m.pignalosa@hotmail.com>
2024-11-25 20:32:33 +1030

    Last edits to sizing

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ document.addEventListener("DOMContentLoaded", initialiseSlider);
 function initialiseSlider() {
     if (slides.length > 0) {
         slides[slideIndex].classList.add("displaySlide");
-        intervalId = setInterval(nextSlide, 5000);
     }
 }
 function showSlide(index) {
@@ -25,14 +24,19 @@ function showSlide(index) {
     });
     slides[slideIndex].classList.add("displaySlide");
 }
+
 function prevSlide() {
     clearInterval(intervalId);
     slideIndex--;
     showSlide(slideIndex);
 }
+
 function nextSlide() {
     slideIndex++;
     showSlide(slideIndex);
 }
 
-// Instagram Image Slider
+
+
+
+
